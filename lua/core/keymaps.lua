aaf45225fcd0de04f444c57ee5cbaa822e186713
M. Mahdi Khosravi <mahdi.khosravi@metu.edu.tr>
2022-12-25 18:26:50 +0300

    fixed tab and added cool keymappings

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -3,13 +3,26 @@ vim.g.mapleader = ' '
 local keymap = vim.keymap.set
 local opts = { silent = true }
 
-
-
 -- Easier split navigation
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Awesome keymappings from theprimagen
+-- Move highlighted blocks 
+keymap("v", "J", ":m '>+1<CR>gv=gv")
+keymap("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Keep cursor in middle of page when half-page jumping
+keymap("n", "<C-d>", "<C-d>zz")
+keymap("n", "<C-u>", "<C-u>zz")
+
+-- Best remap, preserves the highlighted & yanked text in register
+keymap("x", "<leader>p", "\"_dP")
+
+-- Search and replace word under cursor
+keymap("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
 
 -- Telescope
 local builtin = require('telescope.builtin')
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -50,12 +50,16 @@ cmp.setup({
 		end,
 	},
 
+    completion = {
+        autocomplete = false
+    },
+
 	mapping = cmp.mapping.preset.insert({
-		["<C-k>"] = cmp.mapping.select_prev_item(),
-		["<C-j>"] = cmp.mapping.select_next_item(),
-		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+		["<C-p>"] = cmp.mapping.select_prev_item(),
+		["<C-n>"] = cmp.mapping.select_next_item(),
+		["<C-g>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+		["<C-b>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
 		["<C-e>"] = cmp.mapping({
 			i = cmp.mapping.abort(),
 			c = cmp.mapping.close(),
@@ -64,9 +68,7 @@ cmp.setup({
 		-- Set `select` to `false` to only confirm explicitly selected items.
 		["<CR>"] = cmp.mapping.confirm({ select = true }),
 		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expandable() then
+            if luasnip.expandable() then
 				luasnip.expand()
 			elseif luasnip.expand_or_jumpable() then
 				luasnip.expand_or_jump()
@@ -80,9 +82,7 @@ cmp.setup({
 			"s",
 		}),
 		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
+            if luasnip.jumpable(-1) then
 				luasnip.jump(-1)
 			else
 				fallback()
