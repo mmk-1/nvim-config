b9c907a024296766a4627b113a2da13b436d68ff
Mahdi Khosravi <mmk1776@gmail.com>
2023-08-18 10:58:50 +0300

    lazify cmp

--- init.lua
+++ init.lua
@@ -1,22 +1,22 @@
 -- require
-vim.g.mapleader = ' '
-require "plugins"
-require "core.options"
-require "core.keymaps"
-require "core.autocmds"
-require "lsp"
-require "plugins"
+vim.g.mapleader = " "
+require("plugins")
+require("core.options")
+require("core.keymaps")
+require("core.autocmds")
+require("lsp")
+require("plugins")
 
 -- Set colorscheme
 local colorscheme = "onedark"
 
 -- For onedark theme we must select the style like this :/
-require('onedark').setup {
-  style = 'deep'
-}
-require('onedark').load()
+require("onedark").setup({
+	style = "deep",
+})
+require("onedark").load()
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-  return
+	return
 end
--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -69,6 +69,7 @@ return lazy.setup({
 	-- cmp plugins
 	{
 		"hrsh7th/nvim-cmp",
+    event = "InsertEnter",
 		dependencies = {
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-buffer",
