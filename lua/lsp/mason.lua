038120ce412f8e1a4965007cbb385e2bb1ce3c87
Mahdi <mmk@192.168.1.102>
2023-09-14 20:32:09 +0300

    golang lsp, need to fix formatters

--- lua/lsp/mason.lua
+++ lua/lsp/mason.lua
@@ -1,6 +1,7 @@
 local servers = {
 	"lua_ls",
   "clangd",
+  "gopls",
 }
 
 local settings = {
--- lua/lsp/null-ls.lua
+++ lua/lsp/null-ls.lua
@@ -13,8 +13,10 @@ null_ls.setup({
 	sources = {
 		-- formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 		-- formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.stylua,
-    formatting.clang_format,
+		-- formatting.stylua,
+  --   formatting.clang_format,
+  --   formatting.gofumpt,
+  --   formatting.goimports_reviser
     -- diagnostics.flake8
 	},
 })
