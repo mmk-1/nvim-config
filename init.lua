47e317dfa019325b9ae873d5c5b730f076fd1945
Mahdi Khosravi <mmk1776@gmail.com>
2023-09-21 11:36:57 +0300

    init

--- init.lua
+++ init.lua
@@ -1,22 +1,2 @@
--- require
-vim.g.mapleader = " "
-require("plugins")
-require("core.options")
-require("core.keymaps")
-require("core.autocmds")
-require("lsp")
-require("plugins")
-
--- Set colorscheme
-local colorscheme = "onedark"
-
--- For onedark theme we must select the style like this :/
-require("onedark").setup({
-	style = "deep",
-})
-require("onedark").load()
-
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-	return
-end
+require("user.core")
+require("user.lazy")
---
+++ lua/user/plugins/lsp/mason.lua
@@ -0,0 +1,46 @@
+local M = {
+  "williamboman/mason.nvim",
+  -- commit = "4546dec8b56bc56bc1d81e717e4a935bc7cd6477",
+  cmd = "Mason",
+  event = "BufReadPre",
+  dependencies = {
+    {
+      "williamboman/mason-lspconfig.nvim",
+      -- commit = "93e58e100f37ef4fb0f897deeed20599dae9d128",
+    },
+    { "jayp0521/mason-null-ls.nvim" },
+  },
+}
+
+local settings = {
+  ui = {
+    border = "none",
+    icons = {
+      package_installed = "✓",
+      package_pending = "➜",
+      package_uninstalled = "✗"
+    },
+  },
+  log_level = vim.log.levels.INFO,
+  max_concurrent_installers = 4,
+}
+
+function M.config()
+  require("mason").setup(settings)
+  require("mason-lspconfig").setup {
+    ensure_installed = require("user.utils").servers,
+    automatic_installation = true,
+  }
+  require("mason-null-ls").setup({
+    -- list of formatters & linters for mason to install
+    ensure_installed = {
+      "stylua", -- lua formatter
+      "gofumpt",
+      "goimports-reviser",
+      "golines",
+    },
+    automatic_installation = true,
+  })
+end
+
+return M
