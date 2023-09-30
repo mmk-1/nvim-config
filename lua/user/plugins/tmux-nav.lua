b1485c2193b34d207cd4ba549307f93f27348faf
Mahdi Khosravi <mmk1776@gmail.com>
2023-09-30 13:35:53 +0300

    refactoring

--- lua/user/plugins/tmux-nav.lua
+++ lua/user/plugins/tmux-nav.lua
@@ -1,14 +1,12 @@
 return {
     "alexghergh/nvim-tmux-navigation",
     keys = {
-
+        { "<c-h>", "<cmd>NvimTmuxNavigateLeft<cr>" },
+        { "<c-j>", "<cmd>NvimTmuxNavigateDown<cr>" },
+        { "<c-k>", "<cmd>NvimTmuxNavigateUp<cr>" },
+        { "<c-l>", "<cmd>NvimTmuxNavigateRight<cr>" },
     },
     config = function()
-        local nvim_tmux_nav = require('nvim-tmux-navigation')
-
-        vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
-        vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
-        vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
-        vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+        require('nvim-tmux-navigation')
     end
 }
