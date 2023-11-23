bcf23654cc7f8e00a27ddc8250054dbbdf753269
Mahdi Khosravi <mmk1776@gmail.com>
2023-11-23 12:25:03 +0300

    fixed clang format!

--- lua/user/null-ls.lua
+++ lua/user/null-ls.lua
@@ -9,14 +9,7 @@ function M.config()
 
   null_ls.setup {
     sources = {
-      -- formatting.stylua,
-      -- formatting.prettier,
-      -- formatting.prettier.with {
-      --   extra_filetypes = { "toml" },
-      --   -- extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-      -- },
-      -- null_ls.builtins.diagnostics.eslint,
-      -- null_ls.builtins.completion.spell,
+      formatting.clang_format,
     },
   }
 end
